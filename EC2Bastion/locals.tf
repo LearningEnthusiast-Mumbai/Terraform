d3b6b30477ed60a0c48557c9d6730ce797234a0c
Vikas <vikas2026aws@gmail.com>
2026-09-14 17:11:26 +0530

    Bastion Host example

--- EC2Bastion/locals.tf
+++ EC2Bastion/locals.tf
@@ -1,7 +1,7 @@
 locals {
   environment = var.environment
   owners      = var.business_unit
-  name        = "$(var.environment)-$(var.business_unit)"
+  name        = "${var.environment}-${var.business_unit}"
 
   common_tags = {
     environment = local.environment
